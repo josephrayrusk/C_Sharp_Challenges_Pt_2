e8b767c00f15becdcda24be270d133e49ec75db3
josephrayrusk <35713802+josephrayrusk@users.noreply.github.com>
2018-02-27 21:28:38 -0800

    ChallengePostalCalculatorHelperMethods

---
+++ ChallengePostalCalculatorHelperMethods/ChallengePostalCalculatorHelperMethods/Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengePostalCalculatorHelperMethods.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h3>Postal Calculator</h3>
+            <br />
+            Width: <asp:TextBox ID="widthText" runat="server" OnTextChanged="widthText_TextChanged"></asp:TextBox>
+            <br />
+            Height:
+            <asp:TextBox ID="heightText" runat="server" OnTextChanged="heightText_TextChanged"></asp:TextBox>
+            <br />
+            Length:
+            <asp:TextBox ID="lengthText" runat="server" OnTextChanged="lengthText_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            <asp:RadioButton ID="groundButton" runat="server" AutoPostBack="True" GroupName="shipSetting" OnCheckedChanged="groundButton_CheckedChanged" Text="Ground" />
+            <br />
+            <asp:RadioButton ID="airButton" runat="server" AutoPostBack="True" GroupName="shipSetting" OnCheckedChanged="airButton_CheckedChanged" Text="Air" />
+            <br />
+            <asp:RadioButton ID="nextDayButton" runat="server" AutoPostBack="True" GroupName="shipSetting" OnCheckedChanged="nextDayButton_CheckedChanged" Text="Next Day" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengePostalCalculatorHelperMethods/ChallengePostalCalculatorHelperMethods/Default.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePostalCalculatorHelperMethods
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        protected void widthText_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        protected void groundButton_CheckedChanged(object sender, EventArgs e)
+        {
+            calculateCost();
+        }
+
+        protected void airButton_CheckedChanged(object sender, EventArgs e)
+        {
+            calculateCost();
+        }
+
+        protected void nextDayButton_CheckedChanged(object sender, EventArgs e)
+        {
+            calculateCost();
+        }
+
+        protected void heightText_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        protected void lengthText_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void calculateCost()
+        {         
+            if (!String.IsNullOrEmpty(widthText.Text) && !String.IsNullOrEmpty(heightText.Text))
+            {
+                double width;
+                double height;
+                double length;
+
+                Double.TryParse(widthText.Text, out width);
+                Double.TryParse(heightText.Text, out height);
+                if (!Double.TryParse(lengthText.Text, out length))
+                    length = 1;
+                 
+                
+                double cost= 0.00;
+                double size = width * height * length;
+                    if (groundButton.Checked) cost = size * 0.15;
+                    else if (airButton.Checked) cost = size * 0.25;
+                    else if (nextDayButton.Checked) cost = size * 0.45;
+
+                resultLabel.Text = "Total Cost is " + cost.ToString("C");
+            }
+            else resultLabel.Text = "Please enter Width and Height.";
+        }
+    }
+
+
+}
